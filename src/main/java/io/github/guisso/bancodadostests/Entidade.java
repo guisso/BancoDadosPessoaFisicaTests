e2e5f5192eb248631a2bffd37b582e582f9b49c2
guisso <lguisso@gmail.com>
2022-04-21 11:15:16 -0300

    Entidade

---
+++ src/main/java/io/github/guisso/bancodadostests/Entidade.java
@@ -0,0 +1,44 @@
+/* 
+ * Material didático destinado ao curso de 
+ * Programação Orientada a Objetos
+ * do Bacharelado em Ciência da Computação do IFNMG 
+ * - Câmpus Montes Claros.
+ *
+ * O uso deste material é livre e regido pela licença 
+ * Creative Commons como Atribuição-NãoComercial
+ * -CompartilhaIgual 4.0 Internacional:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package io.github.guisso.bancodadostests;
+
+/**
+ * Aspectos comuns a todas as entidades gerenciadas pelo sistema.
+ *
+ * @author Luis Guisso &lt;luis dot guisso at ifnmg dot edu dot br&gt;
+ * @version 0.1, 18/04/2022
+ */
+public abstract class Entidade {
+
+    // Representação da chave primária padrão para a entidade.
+    private Long id;
+
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    public Entidade() {
+    }
+
+    public Entidade(Long id) {
+        this.id = id;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    //</editor-fold>
+
+}
